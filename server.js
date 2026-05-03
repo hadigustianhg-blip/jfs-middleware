8794838a1044732fe1eb5aaf766aaa12b2b460b4
hadigustianhg-blip <hadi.gustian.hg@gmail.com>
2026-05-03 16:40:13 +0700

    server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-let AUTH_TOKEN = "ISI_TOKEN_KAMU";
+let AUTH_TOKEN = "11e17a31b9494b2f8875e8286666c8fb";
 
 // endpoint update token
 app.post("/set-token", (req, res) => {
