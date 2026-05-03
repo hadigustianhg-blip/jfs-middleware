b0718703c99066b7cfcde501c158b927897ccd53
hadigustianhg-blip <hadi.gustian.hg@gmail.com>
2026-05-03 16:57:30 +0700

    server.js

--- server.js
+++ server.js
@@ -50,9 +50,14 @@ app.get("/jfs-data", async (req, res) => {
   }
 });
 
-// ✅ FIX PORT
+
+app.get("/", (req, res) => {
+  res.send("API JFS Middleware aktif 🚀");
+});
+
+// baru listen
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server jalan di port ${PORT}`);
 });
